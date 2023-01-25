d09c6fc53caa438feaffabe2dd204489641e5428
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-26 02:11:21 +0700

    2

--- 230125/repeat_alpha.c
+++ 230125/repeat_alpha.c
@@ -4,6 +4,8 @@
 int main(int ac, char **av)
 {
 	int i = 0;
+	int j = 0;
+	int k = 0;
 
 	if (ac == 2)
 	{
@@ -11,9 +13,17 @@ int main(int ac, char **av)
 		{
 			if ((av[1][i] >= 'a' && av[1][i] <= 'z') || (av[1][i] >= 'A' && av[1][i] <= 'Z'))
 			{
-				
+				j = av[1][i] - 'a';
+				printf("j = %d\n", j);
+				while (k <= j)
+				{
+					write(1, &av[1][i], 1);
+					k++;
+				}
 			}
+			i++;
 		}
+		write(1, "\n", 1);
 	}
 	else
 		write(1, "\n", 1);
