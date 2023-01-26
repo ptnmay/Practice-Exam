b0a2cf9ebff555bdb1f9712788ea603e1c0ae27a
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-27 03:07:36 +0700

    union

--- 230126/union.c
+++ 230126/union.c
@@ -6,35 +6,60 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 01:48:44 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/01/27 01:54:50 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/01/27 02:58:03 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int ft_check(char *s1, char s, int len)
 {
 	int i = 0;
 
-	while (str)
+	while (s1[i] && (i < len || len == -1))
 	{
+		if (s1[i] == s)
+			return(0);
 		i++;
 	}
-	return(i);
+	return(1);
 }
 
 int main(int ac, char **av)
 {
 	int i = 0;
+	int j = 0;
+	// int	k = 0;
 
 	if (ac == 3)
 	{
 		while (av[1][i])
 		{
-			av[1][ft_strlen(av[1])] = av[2][i];
 			i++;
 		}
-		printf("%s\n", av[1]);
+		while (av[2][j])
+		{
+			av[1][i] = av[2][j];
+			i++;
+			j++;
+		}
+		i = 0;
+		while(av[1][i])
+		{
+			if (ft_check(av[1], av[1][i], i) != 0)
+				write(1, &av[1][i], 1);
+			i++;
+		}
+
+		// while(av[1][k])
+		// {
+		// 	if (ft_check(av[1], av[1][k], k) != 0)
+		// 		write(1, &av[1][k], 1);
+		// 	k++;
+		// }
+		//printf("%s\n", av[1]);
 	}
+	write(1, "\n", 1);
 }
+//mew and fah help
