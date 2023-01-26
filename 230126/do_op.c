14f9efc73eebf14fa6b165fd3fe331b11d26b1d0
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-26 23:39:50 +0700

    level 2

---
+++ 230126/do_op.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_op.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 23:17:07 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/01/26 23:28:40 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	int i = 0;
+
+	if (ac == 4)
+	{
+		if (av[2][i] == '*')
+			printf("%d", atoi(av[1]) * atoi(av[3]));
+		else if (av[2][i] == '+')
+			printf("%d", atoi(av[1]) + atoi(av[3]));
+		else if (av[2][i] == '-')
+			printf("%d", atoi(av[1]) - atoi(av[3]));
+		else if (av[2][i] == '/')
+			printf("%d", atoi(av[1]) / atoi(av[3]));
+	}
+	printf("\n");
+}
