9f966aa8668a29042e2cc22809194b258e79c157
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-01-26 15:06:43 +0700

    rot13

--- 230126/rot_13_no_.c
+++ 230126/rot_13_no_.c
@@ -5,36 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/26 14:53:38 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/01/26 15:05:33 by psaeyang         ###   ########.fr       */
+/*   Created: 2023/01/26 14:18:55 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/01/26 14:53:06 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_putchar(char a)
-{
-	write(1, &a, 1);
-}
-
 int main(int ac, char **av)
 {
 	int i = 0;
-	char *str = av[1];
 
 	if (ac == 2)
 	{
-		while (str[i])
+		while(av[1][i])
 		{
-			if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
-				ft_putchar(str[i] + 13);
-			else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
-				ft_putchar(str[i] - 13);
+			if ((av[1][i] >= 'a' && av[1][i] <= 'm') || (av[1][i] >= 'A' && av[1][i] <= 'M'))
+				write(1, &av[1][i] + 13, 1);
+			else if ((av[1][i] >= 'n' && av[1][i] <= 'z') || (av[1][i] >= 'N' && av[1][i] <= 'Z'))
+				write(1, &av[1][i] - 13, 1);
 			else
-				ft_putchar(str[i]);
+				write(1, &av[1][i], 1);
 			i++;
 		}
+	//	write(1, "\n", 1);
 	}
+	//else
 		write(1, "\n", 1);
 }
+
+//wrong code
