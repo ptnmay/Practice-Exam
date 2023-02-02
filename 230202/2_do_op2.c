6bb064fb54a8e88c8737d09430f23899a355425e
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-02-02 21:56:59 +0700

    do_op

--- 230202/2_do_op2.c
+++ 230202/2_do_op2.c
@@ -1,37 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   2_last_word2.c                                     :+:      :+:    :+:   */
+/*   2_do_op2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 20:26:05 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/02/02 20:46:42 by psaeyang         ###   ########.fr       */
+/*   Created: 2023/02/02 20:53:38 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/02/02 21:54:57 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int ac, char **av)
 {
-	int i = 0;
-
-	if (ac == 2)
+	if (ac == 4)
 	{
-		while(av[1][i])
-			i++;
-		i = i - 1;
-		while(av[1][i] == 32 || (av[1][i] >= 9 && av[1][i] <= 13))
-			i--;
-		while(av[1][i] > 33)
-			i--;
-		i++;
-		while(av[1][i] && av[1][i] != 32)
-		{
-			write(1, &av[1][i], 1);
-			i++;
-		}
+		if (av[2][0] == '+')
+			printf("%d", (atoi(av[1]) + atoi(av[3])));
+		else if (av[2][0] == '-')
+			printf("%d", (atoi(av[1]) - atoi(av[3])));
+		else if (av[2][0] == '*')
+			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		else if (av[2][0] == '/')
+			printf("%d", (atoi(av[1]) / atoi(av[3])));
 	}
-	write(1, "\n", 1);
+	printf("\n");
 }
