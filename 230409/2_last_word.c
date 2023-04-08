dbadcdf1459f6996c0c2709a68dd8470211047df
Mavis <105956506+ptnmay@users.noreply.github.com>
2023-04-09 02:02:02 +0700

    last word

--- 230409/2_last_word.c
+++ 230409/2_last_word.c
@@ -6,7 +6,38 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 01:56:09 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/04/09 01:56:14 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/04/09 02:01:07 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int space(char s)
+{
+    if (s >= 9 && s <= 13 || s == 32)
+        return(1);
+    return(0);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        int i = 0;
+
+        while(av[1][i])
+            i++;
+        i--;
+        while(space(av[1][i]) == 1)
+            i--;
+        while(av[1][i] > 32)
+            i--;
+        i++;
+        while(av[1][i] > 32)
+        {
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+}
