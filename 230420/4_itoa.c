d5dcb4d9a333f8745bf1bf573b9f09aceb7ad771
ptnmay <pittinansaeyang@gmail.com>
2023-04-20 12:24:02 +0700

    itoa

--- 230420/4_itoa.c
+++ 230420/4_itoa.c
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 11:33:33 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/04/20 11:38:57 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/04/20 12:23:50 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ char	*ft_itoa(int nbr)
 {
     int n = nbr;
     int len = 0;
-
     if (nbr <= 0)
         len++;
     while(n)
@@ -29,9 +28,9 @@ char	*ft_itoa(int nbr)
     if (nbr < 0)
     {
         res[0] = '-';
-        nbr = -nbr;
+        nbr = -nbr;    
     }
-    if (nbr = 0)
+    if (nbr == 0)
     {
         res[0] = '0';
         return(res);
@@ -39,7 +38,16 @@ char	*ft_itoa(int nbr)
     while(nbr)
     {
         res[--len] = nbr % 10 + '0';
-        nbr /= 10;
+        nbr = nbr / 10;
     }
     return(res);
+
+}
+
+int main()
+{
+	printf("%s\n", ft_itoa(123));
+	printf("%s\n", ft_itoa(-32));
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(34567));
 }
