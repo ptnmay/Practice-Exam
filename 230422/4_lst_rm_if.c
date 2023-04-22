b3ac945e1695e57b68313a785753016844dd76ab
ptnmay <pittinansaeyang@gmail.com>
2023-04-23 02:31:29 +0700

    lst rm

--- 230422/4_lst_rm_if.c
+++ 230422/4_lst_rm_if.c
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 02:29:13 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/04/22 02:59:11 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/04/22 04:24:02 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,13 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
     if (begin_list == NULL || *begin_list == NULL)
         return ;
-
     t_list *tmp = *begin_list;
-    
-    if (cmp(tmp->data, data_ref) == 0)
+
+    if(cmp(tmp->data, data_ref) == 0)
     {
         *begin_list = tmp->next;
         free(tmp);
-        ft_list_remove_if(begin_list, data_ref, cmp);
+        ft_list_remove_if(tmp->data, data_ref, cmp);
     }
     tmp = *begin_list;
     ft_list_remove_if(&tmp->next, data_ref, cmp);
