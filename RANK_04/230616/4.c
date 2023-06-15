da2d0469c361565d654703d8be678495598df523
ptnmay <pittinansaeyang@gmail.com>
2023-06-16 05:56:02 +0700

    5 times

--- RANK_04/230616/4.c
+++ RANK_04/230616/4.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   3.c                                                :+:      :+:    :+:   */
+/*   4.c                                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/16 04:39:17 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/06/16 05:19:24 by psaeyang         ###   ########.fr       */
+/*   Created: 2023/06/16 05:13:37 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/06/16 05:36:48 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <string.h>
 
 static void perr(char *s) {
@@ -21,9 +21,9 @@ static void perr(char *s) {
 
 static int cd(char **av, int i) {
 	if (i != 2)
-		return(perr("error bad av\n"), 1);
+		return(perr("bad av\n"), 1);
 	else if (chdir(av[1]) == -1)
-		return(perr("error can't change\n"), perr(av[1]), perr("\n"), 1);
+		return(perr("can't change directory to "), perr(av[1]), perr("\n"), 1);
 	return 0;
 }
 
@@ -32,9 +32,8 @@ static int exec(char **av, char **envp, int i) {
 	int fds[2];
 	int pip = (av[i] && !strcmp(av[i], "|"));
 	if (pip && pipe(fds) == -1)
-		return(perr("error fatal\n"), 1);
+		return(perr("fatal\n"), 1);
 	int pid = fork();
-	//try ro remember after this
 	if (!pid) {
 		av[i] = 0;
 		if (pip && (dup2(fds[1], 1) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
@@ -52,7 +51,9 @@ int main(int ac, char **av, char **envp) {
 	(void)ac;
 	int status = 0;
 	while(*av && *(av + 1)) {
+		//forgot this!!!
 		av++;
+		//this!!!
 		int i = 0;
 		while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
--- RANK_04/230616/5.c
+++ RANK_04/230616/5.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   3.c                                                :+:      :+:    :+:   */
+/*   5.c                                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/16 04:39:17 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/06/16 05:19:24 by psaeyang         ###   ########.fr       */
+/*   Created: 2023/06/16 05:37:47 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/06/16 05:54:12 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ static void perr(char *s) {
 
 static int cd(char **av, int i) {
 	if (i != 2)
-		return(perr("error bad av\n"), 1);
+		return(perr("bad av"), 1);
 	else if (chdir(av[1]) == -1)
-		return(perr("error can't change\n"), perr(av[1]), perr("\n"), 1);
+		return(perr("can't cd to "), perr(av[1]), perr("\n"), 1);
 	return 0;
 }
 
@@ -32,15 +32,14 @@ static int exec(char **av, char **envp, int i) {
 	int fds[2];
 	int pip = (av[i] && !strcmp(av[i], "|"));
 	if (pip && pipe(fds) == -1)
-		return(perr("error fatal\n"), 1);
+		return(perr("fatal\n"), 1);
 	int pid = fork();
-	//try ro remember after this
 	if (!pid) {
 		av[i] = 0;
 		if (pip && (dup2(fds[1], 1) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
 			return(perr("fatal\n"), 1);
 		execve(*av, av, envp);
-		return(perr("can't execute"), perr(*av), perr("\n"), 1);
+		return(perr("can't execute\n"), 1);
 	}
 	waitpid(pid, &status, 0);
 	if (pip && (dup2(fds[0], 0) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
@@ -50,12 +49,13 @@ static int exec(char **av, char **envp, int i) {
 
 int main(int ac, char **av, char **envp) {
 	(void)ac;
-	int status = 0;
+	int status= 0;
 	while(*av && *(av + 1)) {
 		av++;
 		int i = 0;
 		while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
+		//if (!strcmp(av[i], "cd")) wrong here
 		if (!strcmp(*av, "cd"))
 			status = cd(av, i);
 		else if (i)
